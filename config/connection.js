8a0367128e103feca00551e85ef21508c84a2aa7
Joseph Palmisano <josephpalmisano2022@u.northwestern.edu>
2022-04-09 13:56:10 -0400

    fixed student route

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
-const { connect, connection } = require('mongoose');
+const { connect, connection } = require("mongoose");
 
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/studentsDB';
+const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/studentsDB";
 
 connect(connectionString, {
   useNewUrlParser: true,
@@ -11,3 +10,5 @@ connect(connectionString, {
 });
 
 module.exports = connection;
+
+// mongodb+srv://atlas:admin@cluster0.wwdq2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
